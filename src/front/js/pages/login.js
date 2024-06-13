49baeeaf424a99a0f223cbe797b8c1541042b15c
Brandon Panavelil <138955239+BrandonPanavelil@users.noreply.github.com>
2024-06-13 01:04:39 +0000

    end of class progress thus far

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Login = () => {
+    const navigate = useNavigate();
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    let token = sessionStorage.getItem("token");
+    console.log("Your token: ", token);
+
+    const handleClick = () => {
+        actions.login(email, password).then(() => {
+            navigate("/");
+        });
+    };
+
+    return (
+        <div className="text-center mt-5">
+            {token && token !== "" && token !== undefined ? (
+                <h1>You are logged in</h1>
+            ) : (
+                <>
+                    <h1>Login</h1>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={handleClick}>Login</button>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+
+  
+    
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -15,17 +15,51 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			]
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
+		// we moved the log in function from login.js to here,
+		// creating an action in flux to handle the process
+		// and allowing us to set the token in the store for
+		// future use.
+
+		login: async (email, password) => {
+			const options = {
+				method: 'POST',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password
+				}),
+			};
+		
+			const response = await fetch('https://expert-waffle-5gqgx5v6v5qgf45v7-3001.app.github.dev/api/token', options);
+		
+			if (!response.ok) {
+				console.log("error: ", response.status, response.statusText);
+				return false;
+			}
+		
+			const data = await response.json();
+			sessionStorage.setItem("token", data.access_token);
+			setStore({ token: data.access_token });
+			return true;
+		},
+		
+		// logout allows removal of the token from the store and sessionStorage
+		logout: () => {
+    		sessionStorage.removeItem("token");
+    		setStore({ token: null });
+    		console.log("You've logged out.");
+		},
+
+		
+		getMessage: async () => {
+			try {
+				// fetching data from the backend
+				const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+				const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
